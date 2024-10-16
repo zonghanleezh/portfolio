591ec82ff0657fb860bce09be92f5b04170665f9
Lee Zong Han <zonghanleezh@gmail.com>
2024-10-16 18:57:56 +0800

    Added theme components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeProvider } from 'next-themes'
 import localFont from 'next/font/local'
 import './globals.css'
 
@@ -19,9 +20,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang='en'>
+        <html lang='en' suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                {children}
+                <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+                    {children}
+                </ThemeProvider>
             </body>
         </html>
     )
