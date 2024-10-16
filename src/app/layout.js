abf6873c93b4470fdbc2d1f22803ff56f9b6413a
Lee Zong Han <zonghanleezh@gmail.com>
2024-10-16 19:06:00 +0800

    Disabled system theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from 'next-themes'
+// import { ThemeProvider } from 'next-themes'
 import localFont from 'next/font/local'
 import './globals.css'
 
@@ -22,9 +22,9 @@ export default function RootLayout({ children }) {
     return (
         <html lang='en' suppressHydrationWarning>
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-                <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-                    {children}
-                </ThemeProvider>
+                {/* <ThemeProvider attribute='class' defaultTheme='system' enableSystem> */}
+                {children}
+                {/* </ThemeProvider> */}
             </body>
         </html>
     )
