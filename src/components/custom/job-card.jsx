c5a764604cea41f9d2c67cf94a48f8a59060c78a
Lee Zong Han <zonghanleezh@gmail.com>
2024-10-20 23:01:06 +0800

    Removed shadow and added card outline and highlighted hover badges

--- src/components/custom/job-card.jsx
+++ src/components/custom/job-card.jsx
@@ -7,7 +7,7 @@ export default function JobCard({ title, date, content, url, badges }) {
             href={url}
             target='_blank'
             rel='noopener noreferrer'
-            className='block p-6 bg-card text-card-foreground rounded-lg border border-transparent shadow-sm hover:bg-accent hover:text-accent-foreground hover:border-accent hover:shadow-md transition-all duration-200 cursor-pointer group'
+            className='block p-6 bg-background text-foreground rounded-lg border border-border hover:bg-accent hover:text-accent-foreground transition-all duration-200 cursor-pointer group'
         >
             <div className='mb-2'>
                 <h3 className='text-lg font-semibold flex items-center'>
@@ -19,7 +19,11 @@ export default function JobCard({ title, date, content, url, badges }) {
             <p className='mb-4 text-sm'>{content}</p>
             <div className='flex flex-wrap gap-2'>
                 {badges.map((badge, index) => (
-                    <Badge key={index} variant='secondary'>
+                    <Badge
+                        key={index}
+                        variant='secondary'
+                        className='group-hover:bg-background group-hover:text-foreground transition-colors duration-200'
+                    >
                         {badge}
                     </Badge>
                 ))}
