746635ab5fbd2ef4c470ec5baa363641b774f894
Lee Zong Han <zonghanleezh@gmail.com>
2024-11-27 23:36:07 +0800

    Abstracted footer

---
+++ src/components/custom/footer.jsx
@@ -0,0 +1,52 @@
+import Link from 'next/link'
+
+export function Footer() {
+    return (
+        <footer className='mt-16'>
+            <div>
+                <span className='text-muted-foreground'>
+                    Coded in{' '}
+                    <Link
+                        href='https://code.visualstudio.com/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='text-neutral-800 hover:text-neutral-600 no-underline'
+                    >
+                        Visual Studio Code
+                    </Link>
+                    . Built with{' '}
+                    <Link
+                        href='https://nextjs.org/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='text-neutral-800 hover:text-neutral-600 no-underline'
+                    >
+                        Next.js
+                    </Link>{' '}
+                    and{' '}
+                    <Link
+                        href='https://tailwindcss.com/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='text-neutral-800 hover:text-neutral-600 no-underline'
+                    >
+                        Tailwind CSS
+                    </Link>
+                    . Deployed with{' '}
+                    <Link
+                        href='https://railway.app/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        className='text-neutral-800 hover:text-neutral-600 no-underline'
+                    >
+                        Railway
+                    </Link>
+                    .
+                </span>
+            </div>
+            <div className='mt-4 text-muted-foreground'>
+                <span>© {new Date().getFullYear()} Lee Zong Han</span>
+            </div>
+        </footer>
+    )
+}
