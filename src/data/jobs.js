639bf55cf1ac59ff7694031ffa56e77aa94d71c6
Lee Zong Han <zonghanleezh@gmail.com>
2024-11-27 23:37:39 +0800

    Updated content

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -3,7 +3,7 @@ export const jobs = [
         title: 'Software Engineer ⋅ Capata',
         date: 'Sep 2024 — Present',
         content:
-            'Spearheaded the business requirements gathering for the joint-venture supply chain financing platform. Collaborated with the joint-venture partner CTO to decide platform requirements and architecture.',
+            'Led the gathering of business requirements for a supply chain financing platform developed in partnership with Capata’s joint-venture. Collaborated closely with the CTO to shape the platform’s technical architecture and define key features.',
         url: 'https://capata.sg/',
         badges: ['Figma'],
     },
@@ -11,7 +11,7 @@ export const jobs = [
         title: 'Software Engineer ⋅ TransAsia Private Capital',
         date: 'May 2023 — Present',
         content:
-            'Architected, built and maintained an end-to-end private debt investment management platform. Worked closely with the investment and middle office to develop tools that streamline the lending process.',
+            'Developed a full-stack private debt investment management platform, optimizing workflows for operations and investment teams. Features include asynchronous trade booking for concurrent processing, robust data validation for accuracy, and simultaneous read / write operations for seamless user experience. The platform also offers real-time reporting, enhancing decision-making for stakeholders.',
         url: 'https://taprivatecapital.com/',
         badges: ['React', 'Express', 'Node.js', 'MongoDB', 'Docker'],
     },
