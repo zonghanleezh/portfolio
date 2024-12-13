0919390aab81436785f7e8b43c32b842ce6c306f
Lee Zong Han <zonghanleezh@gmail.com>
2024-12-13 14:01:56 +0800

    Abstracted supabase bucketUrl

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,30 @@
+const bucketUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+
 export const projects = [
     {
         title: 'Investment Management Platform',
         description:
             'A platform featuring asynchronous trade booking, self-service reporting, portfolio management, and role-based authorisation, enabling efficient workflows for operations and investment teams.',
-        siteUrl: 'https://investment.demo.leezonghan.com',
+        siteUrl: `${process.env.INVESTMENT_DEMO_URL}`,
         images: [
             {
-                src: 'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/projects/investment/login.jpg?t=2024-12-13T02%3A03%3A37.932Z',
+                src: `${bucketUrl}/storage/v1/object/public/portfolio-assets/projects/investment/login.jpg`,
                 alt: 'Login',
             },
             {
-                src: 'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/projects/investment/create_user.jpg?t=2024-12-13T02%3A01%3A21.070Z',
+                src: `${bucketUrl}/storage/v1/object/public/portfolio-assets/projects/investment/create_user.jpg`,
                 alt: 'Create User',
             },
             {
-                src: 'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/projects/investment/holdings.jpg?t=2024-12-13T02%3A03%3A32.406Z',
+                src: `${bucketUrl}/storage/v1/object/public/portfolio-assets/projects/investment/holdings.jpg`,
                 alt: 'Holdings',
             },
             {
-                src: 'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/projects/investment/statement_single.jpg?t=2024-12-13T02%3A03%3A50.715Z',
+                src: `${bucketUrl}/storage/v1/object/public/portfolio-assets/projects/investment/statement_single.jpg`,
                 alt: 'Statement of Account',
             },
             {
-                src: 'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/projects/investment/statement_multi.jpg?t=2024-12-13T02%3A03%3A43.729Z',
+                src: `${bucketUrl}/storage/v1/object/public/portfolio-assets/projects/investment/statement_multi.jpg`,
                 alt: 'Statement of Account Multiple Download',
             },
         ],
--- src/data/urls.js
+++ src/data/urls.js
@@ -1,5 +1,5 @@
-export const profilePictureUrl =
-    'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/profile/profile_picture.jpg?t=2024-12-13T01%3A59%3A46.953Z'
+const bucketUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 
-export const resumeUrl =
-    'https://dgljpdsawrqjifikrckk.supabase.co/storage/v1/object/public/portfolio-assets/resume/Lee_Zong_Han_Resume.pdf?t=2024-12-13T02%3A00%3A11.690Z'
+export const profilePictureUrl = `${bucketUrl}/storage/v1/object/public/portfolio-assets/profile/profile_picture.jpg`
+
+export const resumeUrl = `${bucketUrl}/storage/v1/object/public/portfolio-assets/resume/Lee_Zong_Han_Resume.pdf`
