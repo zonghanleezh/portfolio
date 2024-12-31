b4dcedfb4cdd00efc30389d9ae058c3c4a640aa1
Lee Zong Han <zonghanleezh@gmail.com>
2024-12-31 12:30:35 +0800

    Reverted bullets to content

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -2,24 +2,18 @@ export const jobs = [
     {
         title: 'Software Engineer ⋅ Capata',
         date: 'Sep 2024 — Present',
-        intro: 'Leveraged technical expertise to drive the development of a supply chain financing platform startup, a strategic intiative wholly owned by TransAsia Private Capital.',
-        bullets: [
-            'Led the business requirements gathering, defined key features and designed tailored solutions.',
-            'Collaborated closely with the CTO to shape the platform’s technical architecture.',
-            'Designed the NoSQL database to handle scalable, client relationship management and real-time supply chain financing data',
+        paragraphs: [
+            'A strategic intiative wholly-owned by TransAsia Private Capital.',
+            "Led the business requirements gathering to define the MVP scope. Collaborated with the CTO to shape the platform's technical architecture. Designed the NoSQL database to handle scalable, client relationship management and real-time supply chain financing data.",
         ],
+        url: 'https://capata.sg/',
         badges: ['Next.js', 'MongoDB', 'Figma'],
     },
     {
         title: 'Software Engineer ⋅ TransAsia Private Capital',
         date: 'May 2023 — Present',
-        intro: 'Developed and launched a full-stack private debt investment management platform.',
-        bullets: [
-            'Defined and established the tech stack, core architecture and MVP.',
-            'Optimised workflows for the operations and investment teams.',
-            'Implemented asynchronous trade booking for concurrent processing.',
-            'Designed robust data validation mechanisms for accuracy.',
-            'Developed real-time reporting features for enhanced decision-making.',
+        paragraphs: [
+            'Designed, built and launched a full-stack private debt investment management platform. Implemented asynchronous trade booking for concurrent processing and developed real-time reporting features for bookkeeping and client relationship management.',
         ],
         url: 'https://taprivatecapital.com/',
         badges: ['React', 'Express', 'Node.js', 'MongoDB', 'Docker'],
@@ -27,10 +21,8 @@ export const jobs = [
     {
         title: 'Data Analyst ⋅ Micron Technology',
         date: 'Jul 2022 — Feb 2023',
-        intro: 'Developed python scripts to execute SQL queries, distributing scheduled dashboards.',
-        bullets: [
-            'Applied data analysis techniques on experimental data to resolve production issues.',
-            'Improved manufacturing yield and lowered costs by $80,000/week.',
+        paragraphs: [
+            'Developed python scripts to execute database queries, create reports and automate distribution to stakeholders.',
         ],
         url: 'https://micron.com/',
         badges: ['Python', 'SQL'],
